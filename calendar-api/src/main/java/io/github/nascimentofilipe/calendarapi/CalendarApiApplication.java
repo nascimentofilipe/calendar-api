94f9eb916a3b1a278c2f51ef2d3bedc43ba14774
filipe.soares <filipebnks@gmail.com>
2024-01-22 09:37:32 -0300

    feat: starting project

---
+++ calendar-api/src/main/java/io/github/nascimentofilipe/calendarapi/CalendarApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.nascimentofilipe.calendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarApiApplication.class, args);
+	}
+
+}
